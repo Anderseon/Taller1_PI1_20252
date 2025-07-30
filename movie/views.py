5ee77e8bb6711903f38d2996974afb7beda32160
Jhon Anderson Marín González <jamaring.2003@gmail.com>
2025-07-30 09:53:00 -0500

    Cambios en la función home.

--- movie/views.py
+++ movie/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse('<h1> Welcome to Home page </h1>')
     # return render(request, 'home.html')
-    return render(request, "home.html" , {'name' : 'Greg Lim'})
+    return render(request, "home.html" , {'name' : 'Jhon Anderson Marín González'})
 def about(request):
     return HttpResponse('<h1> Welcome to About Page</h1>')
